f4c0ed5693d62d47818a205013339458f9bd0676
Danny Tuppeny <danny@tuppeny.com>
2020-06-28 11:26:46 +0100

    Split icon generation into two scripts + fix for grind.dart

--- tool/colors/update_colors.dart
+++ tool/colors/update_colors.dart
@@ -5,59 +5,46 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'flutter/colors_cupertino.dart';
-import 'flutter/colors_material.dart';
-import 'generated/colors_cupertino.dart' as cupertino;
-import 'generated/colors_material.dart' as material;
-import 'stubs.dart';
-
 void main(List<String> args) async {
-  // TODO(dantup): Split into two scripts instead of using a flag? It's allow
-  // easier debugging of the second step.
-  if (args.contains("--generate-properties")) {
-    generatePropertiesFiles();
-  } else {
-    await generateDartFiles();
-    // Re-run the script with `--generate-properties` so it can load the files
-    // we generated.
-    final results = await Process.run(Platform.executable,
-        [Platform.script.toFilePath(), "--generate-propertes"]);
-    stdout.write(results.stdout);
-    stderr.write(results.stderr);
-  }
+  await downloadDartFiles();
 }
 
-void generateDartFiles() async {
+void downloadDartFiles() async {
+  File materialFile = File('tool/colors/flutter/colors_material.dart');
+  File cupertinoFile = File('tool/colors/flutter/colors_cupertino.dart');
   // download material/colors.dart and cupertino/colors.dart
-  String materialData =
-      await downloadUrl('https://raw.githubusercontent.com/flutter/flutter/'
-          'master/packages/flutter/lib/src/material/colors.dart');
-  String cupertinoData =
-      await downloadUrl('https://raw.githubusercontent.com/flutter/flutter/'
-          'master/packages/flutter/lib/src/cupertino/colors.dart');
+  await Future.wait([
+    downloadFile(
+        'https://raw.githubusercontent.com/flutter/flutter/'
+        'master/packages/flutter/lib/src/material/colors.dart',
+        materialFile),
+    downloadFile(
+        'https://raw.githubusercontent.com/flutter/flutter/'
+        'master/packages/flutter/lib/src/cupertino/colors.dart',
+        cupertinoFile)
+  ]);
 
   // parse into metadata
-  List<String> materialColors = extractColorNames(materialData);
-  List<String> cupertinoColors = extractColorNames(cupertinoData);
+  List<String> materialColors = extractColorNames(materialFile);
+  List<String> cupertinoColors = extractColorNames(cupertinoFile);
 
   // generate .properties files
   generateDart(materialColors, 'colors_material.dart', 'Colors');
   generateDart(cupertinoColors, 'colors_cupertino.dart', 'CupertinoColors');
 }
 
-void generatePropertiesFiles() {
-  final output = '../../resources/flutter/';
-  generateProperties(material.colors, '$output/colors.properties');
-  generateProperties(cupertino.colors, '$output/cupertino_colors.properties');
-}
-
-Future<String> downloadUrl(String url) async {
+Future<void> downloadFile(String url, File file) async {
+  RegExp imports = new RegExp(r'(?:^import.*;\n{1,})+', multiLine: true);
   HttpClient client = new HttpClient();
   try {
     HttpClientRequest request = await client.getUrl(Uri.parse(url));
     HttpClientResponse response = await request.close();
     List<String> data = await utf8.decoder.bind(response).toList();
-    return data.join('');
+    String contents =
+        data.join('').replaceFirst(imports, "import '../stubs.dart';\n\n");
+
+    file.writeAsStringSync(
+        '/// This file was downloaded by update_colors.dart.\n\n$contents');
   } finally {
     client.close();
   }
@@ -68,7 +55,8 @@ Future<String> downloadUrl(String url) async {
 //   'static const MaterialColor cyan = MaterialColor('
 final RegExp regexp = new RegExp(r'static const \w*Color (\S+) = \w*Color\(');
 
-List<String> extractColorNames(String data) {
+List<String> extractColorNames(File file) {
+  String data = file.readAsStringSync();
   return regexp.allMatches(data).map((Match match) => match.group(1)).toList();
 }
 
@@ -88,64 +76,7 @@ final Map<String, Color> colors = <String, Color>{''');
 
   buf.writeln('};');
 
-  new File('generated/$filename').writeAsStringSync(buf.toString());
+  new File('tool/colors/generated/$filename').writeAsStringSync(buf.toString());
 
-  print('wrote generated/$filename');
-}
-
-void generateProperties(Map<String, Color> colors, String filename) {
-  const validShades = [
-    50,
-    100,
-    200,
-    300,
-    350,
-    400,
-    500,
-    600,
-    700,
-    800,
-    850,
-    900
-  ];
-  StringBuffer buf = StringBuffer();
-  buf.writeln('# Generated file - do not edit.');
-  buf.writeln();
-  buf.writeln('# suppress inspection "UnusedProperty" for whole file');
-
-  for (String name in colors.keys) {
-    Color color = colors[name];
-    if (color is MaterialColor) {
-      buf.writeln('$name.primary=${color}');
-      for (var shade in validShades) {
-        if (color[shade] != null) {
-          buf.writeln('$name[$shade]=${color[shade]}');
-        }
-      }
-    } else if (color is MaterialAccentColor) {
-      buf.writeln('$name.primary=${color}');
-      for (var shade in validShades) {
-        if (color[shade] != null) {
-          buf.writeln('$name[$shade]=${color[shade]}');
-        }
-      }
-    } else if (color is CupertinoDynamicColor) {
-      buf.writeln('$name=${color.color}');
-      buf.writeln('$name.darkColor=${color.darkColor}');
-      buf.writeln('$name.darkElevatedColor=${color.darkElevatedColor}');
-      buf.writeln('$name.darkHighContrastColor=${color.darkHighContrastColor}');
-      buf.writeln(
-          '$name.darkHighContrastElevatedColor=${color.darkHighContrastElevatedColor}');
-      buf.writeln('$name.elevatedColor=${color.elevatedColor}');
-      buf.writeln('$name.highContrastColor=${color.highContrastColor}');
-      buf.writeln(
-          '$name.highContrastElevatedColor=${color.highContrastElevatedColor}');
-    } else {
-      buf.writeln('$name=$color');
-    }
-  }
-
-  new File(filename).writeAsStringSync(buf.toString());
-
-  print('wrote $filename');
+  print('wrote tool/colors/generated/$filename');
 }
